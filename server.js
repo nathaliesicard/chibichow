47af55965f1e55c73eeb0aecb79865a050fe0356
nathaliesicard <nathalie.sicard@gmail.com>
2015-03-22 13:01:04 -0600

    Get web server working

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-console.log("hello world!");
+var koa = require('koa');
+var static = require('koa-static');
+var app = koa();
+
+
+app.use(static('public'));
+
+
+app.use(function *(){
+    this.response.status = 404;
+    this.body = 'File not found! Please go to chibichai.com and start again';
+});
+
+
+var port = 4000;
+
+app.listen(port, function() {
+    console.log('Listening on port ' + port + ', go to http://localhost:' + port + '/ ');
+});
